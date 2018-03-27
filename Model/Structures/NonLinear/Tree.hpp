a88a4f5b6819b8567a4c4f7230c2abb845ab5edd
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2018-03-27 09:50:33 -0600

    Made a Tree class and added code into it.

---
+++ Model/Structures/NonLinear/Tree.hpp
@@ -0,0 +1,39 @@
+//
+//  Tree.hpp
+//  DataStructures
+//
+//  Created by Barnes, Cade on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Tree_hpp
+#define Tree_hpp
+
+#include <stdio.h>
+#include "../../Nodes/BinaryTreeNode.hpp"
+
+template <class Type>
+class Tree
+{
+protected:
+    BinaryTreeNode<Type> * root;
+public:
+    //Informational Methods
+    virtual int getSize() = 0;
+    virtual int getHeight() = 0;
+    virtual bool isComplete() = 0;
+    virtual bool iBalanced() = 0;
+    //*Data Methods*
+    virtual void insert(Type data) = 0;
+    virtual bool contains(Type data) = 0;
+    virtual void remove(Type data) = 0;
+    //*Traversals*
+    virtual void inOrderTraversal() = 0;
+    virtual void preOrderTraversal() = 0;
+    virtual void postOrderTraversal() = 0;
+};
+
+
+
+
+#endif /* Tree_hpp */
